1de45626e2f9af9c3d87c251df4a9bd3f4337b02
Anuj Vaishnav <anujvaishnav20@gmail.com>
2019-05-08 14:55:34 +0100

    changed tabs to space

--- mandelbrot_sw.c
+++ mandelbrot_sw.c
@@ -5,44 +5,44 @@ int createWindow(int width, int height)
   unsigned long black, white;
   
   //Open Display
-	dis = XOpenDisplay(getenv("DISPLAY"));
-	if (dis == NULL) {
-		printf("Couldn't open display.\n");
-		return -1;
-	}
+  dis = XOpenDisplay(getenv("DISPLAY"));
+  if (dis == NULL) {
+    printf("Couldn't open display.\n");
+    return -1;
+  }
 
-	//Create Window
-	int const x = 0, y = 0, border_width = 1;
-	int screen    = DefaultScreen(dis);
-	Window ro     = DefaultRootWindow(dis);
-	black = BlackPixel(dis,screen),	/* get color black */
-	white = WhitePixel(dis, screen);  /* get color white */
-	
-	win = XCreateSimpleWindow(dis, ro, x, y, width, height, border_width, 
+  //Create Window
+  int const x = 0, y = 0, border_width = 1;
+  int screen    = DefaultScreen(dis);
+  Window ro     = DefaultRootWindow(dis);
+  black = BlackPixel(dis,screen),  /* get color black */
+  white = WhitePixel(dis, screen);  /* get color white */
+  
+  win = XCreateSimpleWindow(dis, ro, x, y, width, height, border_width, 
                               black, white);
                               
-	XMapWindow(dis, win); //Make window visible
-	XStoreName(dis, win, "Mandelbrot"); // Set window title
-	
-	/* this routine determines which types of input are allowed in
-	   the input.  see the appropriate section for details...
-	*/
-	XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
-	
-	/* create the Graphics Context */
-	gc = XCreateGC(dis, ro, 0, NULL);
-	
-	/* here is another routine to set the foreground and background
-	   colors _currently_ in use in the window.
-	*/
-	XSetBackground(dis, gc, white);
-	XSetForeground(dis, gc, black);
-	
-	/* clear the window and bring it on top of the other windows */
-	XClearWindow(dis, win);
-	XMapRaised(dis, win);
-	
-	return 0;
+  XMapWindow(dis, win); //Make window visible
+  XStoreName(dis, win, "Mandelbrot"); // Set window title
+  
+  /* this routine determines which types of input are allowed in
+     the input.  see the appropriate section for details...
+  */
+  XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
+  
+  /* create the Graphics Context */
+  gc = XCreateGC(dis, ro, 0, NULL);
+  
+  /* here is another routine to set the foreground and background
+     colors _currently_ in use in the window.
+  */
+  XSetBackground(dis, gc, white);
+  XSetForeground(dis, gc, black);
+  
+  /* clear the window and bring it on top of the other windows */
+  XClearWindow(dis, win);
+  XMapRaised(dis, win);
+  
+  return 0;
 }
 
 int buildColor(int red, int green, int blue)
@@ -55,16 +55,16 @@ int buildColor(int red, int green, int blue)
 
 void drawPixel(int x, int y, int color)
 {
-	XSetForeground(dis, gc, color);
-	XDrawPoint(dis, win, gc, x, y);
+  XSetForeground(dis, gc, color);
+  XDrawPoint(dis, win, gc, x, y);
 }
 
 void close_display()
 {
   XFreeGC(dis, gc);
-	XDestroyWindow(dis, win);
-	XCloseDisplay(dis);
-	exit(1);
+  XDestroyWindow(dis, win);
+  XCloseDisplay(dis);
+  exit(1);
 }
 
 int mandelbrot(uint32_t ImageWidth, uint32_t ImageHeight, uint32_t MaxIterations, 
@@ -140,11 +140,11 @@ void main()
   {
     printf("created window\n");
     
-/*	  XEvent ev;*/
+/*    XEvent ev;*/
     
-    XEvent event;		/* the XEvent declaration !!! */
-	  KeySym key;		/* a dealie-bob to handle KeyPress Events */	
-	  char text[255];		/* a char buffer for KeyPress Events */
+    XEvent event;    /* the XEvent declaration !!! */
+    KeySym key;    /* a dealie-bob to handle KeyPress Events */  
+    char text[255];    /* a char buffer for KeyPress Events */
     
     bool quit = false;
     bool zoom_on = false;
@@ -153,60 +153,60 @@ void main()
       if(XCheckWindowEvent(dis, win, KeyPressMask, &event))
       {
         if(event.type == KeyPress
-		       && XLookupString(&event.xkey, text, 255, &key, 0) == 1) 
+           && XLookupString(&event.xkey, text, 255, &key, 0) == 1) 
         {
-		      /* use the XLookupString routine to convert the invent
-		         KeyPress data into regular text.  Weird but necessary...
-		      */
-			    if (text[0]=='q') 
-				    quit = true;
-				    
-				  switch(text[0])
-				  {
-				    // quit
-				    case 'q': 
-				      quit = true;
-				      break;
-				    // toggle zoom
-				    case 'z': 
-				      zoom_on = !zoom_on;
-				      break;
-				    // reset
-				    case 'r': 
-				      zoom = 1;
-				      zoom_on = false;
-				      
-				      cRe = -1.25;
+          /* use the XLookupString routine to convert the invent
+             KeyPress data into regular text.  Weird but necessary...
+          */
+          if (text[0]=='q') 
+            quit = true;
+            
+          switch(text[0])
+          {
+            // quit
+            case 'q': 
+              quit = true;
+              break;
+            // toggle zoom
+            case 'z': 
+              zoom_on = !zoom_on;
+              break;
+            // reset
+            case 'r': 
+              zoom = 1;
+              zoom_on = false;
+              
+              cRe = -1.25;
               cIm = -0.18;
-				      break;
-				    // Move left and right  
-				    case 'a':
-				      cRe -= 0.1;
-				      break;
-			      case 'd':
-			        cRe += 0.1;
-				      break;
-				    case 'w':
-				      cIm += 0.1;
-				      break;
-				    case 's': 
-		          cIm -= 0.1;
-				      break;
-				      
-				    default:
-				      break;
-				  } // switch
-		    } // if key press
+              break;
+            // Move left and right  
+            case 'a':
+              cRe -= 0.1;
+              break;
+            case 'd':
+              cRe += 0.1;
+              break;
+            case 'w':
+              cIm += 0.1;
+              break;
+            case 's': 
+              cIm -= 0.1;
+              break;
+              
+            default:
+              break;
+          } // switch
+        } // if key press
       } // if
-			
-			// continue drawing otherwise
-	    mandelbrot(ImageWidth, ImageHeight, MaxIterations, cRe, cIm, zoom);
-		  
-		  if(zoom_on)
-		    zoom++;
-		  
-	  } // while
-	  
+      
+      // continue drawing otherwise
+      mandelbrot(ImageWidth, ImageHeight, MaxIterations, cRe, cIm, zoom);
+      
+      if(zoom_on)
+        zoom++;
+      
+    } // while
+    
     close_display();
   } // if
   else
